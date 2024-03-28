00f5b7cd9c7ae84aabd8ce7a2ab689bac05ef740
David Douangmarath <david.douangmarath@etu.univ-lyon1.fr>
2024-03-28 11:41:46 +0100

    Fix numéro 1 des pipelines

--- src/main/java/model/Pacman.java
+++ src/main/java/model/Pacman.java
@@ -1,22 +1,42 @@
 package model;
 
+/**
+ * Classe Pacman qui contient la position de Pacman
+ * et qui implémente la logique de mouvement
+ */
 public class Pacman {
     public int direction = 0;
     public int pacmanX;
     public int pacmanY;
 
-    public Pacman(int x0,int y0){
+    /**
+     * Constructeur pour la classe Pacman
+     * @param x0
+     * @param y0
+     */
+
+    public Pacman(final int x0,final int y0){
         pacmanX = x0;
         pacmanY = y0;
     }
 
+    /**
+     * Méthode pour le mouvement de pacman en fonction
+     * de la direction
+     */
+
     public void move() {
-        if(direction == 0) pacmanX++;
-        if(direction == 1) pacmanY++;
-        if(direction == 2) pacmanX--;
-        if(direction == 3) pacmanY--;
+        if (direction == 0) pacmanX++;
+        if (direction == 1) pacmanY++;
+        if (direction == 2) pacmanX--;
+        if (direction == 3) pacmanY--;
 
     }
+
+    /**
+     * Setter pour la direction
+     * @param direction
+     */
 
     public void setDirection(int direction) {
         this.direction = direction;
