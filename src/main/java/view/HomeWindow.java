8642de4942b4f8214fbd82872f6e392b0069061d
Camille <camille.morfin@etu.univ-lyon1.fr>
2024-04-04 11:12:28 +0200

    Mise en page de Home

--- src/main/java/view/HomeWindow.java
+++ src/main/java/view/HomeWindow.java
@@ -1,53 +1,107 @@
 package view;
 
+import java.awt.Color;
 import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
 
+import javax.swing.BorderFactory;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.border.LineBorder;
 
 /**
  * 
  */
 public class HomeWindow extends JFrame implements ActionListener {
-    
+
     /**
      * 
      */
     public HomeWindow() {
         this.setTitle("Pac Man");
-        //this.setIconImage(new ImageIcon("src/icone.png").getImage());
+        // this.setIconImage(new ImageIcon("src/icone.png").getImage());
 
         this.setSize(700, 500);
         this.setResizable(false);
         this.setLocationRelativeTo(null);
 
         this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-        
+
         this.setContentPane(this.createPanel());
+        try {
+            File fontStyle = new File("src/main/ressources/fonts/PAC-FONT.TTF");
+            Font font = Font.createFont(Font.TRUETYPE_FONT, fontStyle).deriveFont(50f);
+            labelTitre.setFont(font);
+            labelTitre.setForeground(
+                    new Color(255, 255, 0));
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        this.getContentPane().setBackground(
+                new Color(0, 0, 0));
     }
 
     private JPanel panel;
+    private JPanel panelButton;
     private JButton buttonStart;
     private JButton buttonExit;
+    private JLabel labelTitre;
 
     /**
      * @return JPanel
      */
     private JPanel createPanel() {
         panel = (JPanel) getContentPane();
-        panel.setLayout(new FlowLayout());
+        panel.setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.gridx = 0;
+        c.gridy = 0;
+        c.gridwidth = 3;
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.anchor = GridBagConstraints.PAGE_START;
+
+        labelTitre = new JLabel("Pac-Man");
+        panel.add(labelTitre, c);
+
+        // Boutons panel
+        panelButton = new JPanel();
+        panelButton.setLayout(new FlowLayout());
+        panelButton.setBackground(Color.BLACK);
 
         buttonStart = new JButton("Jouer");
         buttonStart.addActionListener(this);
-        panel.add(buttonStart);
+        buttonStart.setForeground(
+                new Color(255, 255, 0));
+        buttonStart.setBackground(Color.BLUE);
+        panelButton.add(buttonStart);
 
         buttonExit = new JButton("Quitter");
         buttonExit.addActionListener(this);
-        panel.add(buttonExit);
+        buttonExit.setForeground(
+                new Color(255, 255, 0));
+        buttonExit.setBackground(Color.BLACK);
+        buttonExit.setBorder(BorderFactory.createCompoundBorder(
+                new LineBorder(Color.BLUE, 2),
+                new EmptyBorder(10, 20, 10, 20)));
+        panelButton.add(buttonExit);
 
+        buttonStart.setPreferredSize(buttonExit.getPreferredSize());
+
+        c.gridx = 0;
+        c.gridy = 1;
+        c.gridwidth = 3;
+        c.fill = GridBagConstraints.NONE;
+        c.anchor = GridBagConstraints.CENTER;
+        panel.add(panelButton, c);
 
         /* Instructions pour créer des widgets */
 
@@ -64,4 +118,5 @@ public class HomeWindow extends JFrame implements ActionListener {
             this.dispose();
         }
     }
+
 }
