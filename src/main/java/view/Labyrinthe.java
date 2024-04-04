b266188670043518b636ed80e05edb63cfbc2228
David Douangmarath <david.douangmarath@etu.univ-lyon1.fr>
2024-04-04 11:19:37 +0200

    Modification de la methode keyPressed pour mettre à jour le model lorsqu'une touche est appuyée

--- src/main/java/view/Labyrinthe.java
+++ src/main/java/view/Labyrinthe.java
@@ -1,4 +1,7 @@
 package view;
+import controller.PacmanController;
+import model.Pacman;
+
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
@@ -10,10 +13,15 @@ import javax.swing.JPanel;
  * @return Le fenetre principal. créé, avec le labyrinthe chargé.
  */
 public class Labyrinthe extends JFrame implements KeyListener {
+    private PacmanController controller;
+    private Pacman pacman;
     /**
  * Constructeur de la classe Labyrinthe.
  */
-    public Labyrinthe() {
+    public Labyrinthe(PacmanController controller, Pacman pacman) {
+        this.controller = controller;
+        this.pacman = pacman;
+
         this.setContentPane(this.createPanel());
         this.setTitle("Pacman");
         this.setSize(700, 500);
@@ -57,8 +65,22 @@ public class Labyrinthe extends JFrame implements KeyListener {
 
     @Override
     public final void keyPressed(final KeyEvent e) {
-        if (e.getKeyCode() == KeyEvent.VK_ESCAPE) {
-            menuPause();
+        switch(e.getKeyCode()) {
+            case KeyEvent.VK_UP:
+                controller.handleMovement(Pacman.Direction.UP);
+                break;
+            case KeyEvent.VK_DOWN:
+                controller.handleMovement(Pacman.Direction.DOWN);
+                break;
+            case KeyEvent.VK_LEFT:
+                controller.handleMovement(Pacman.Direction.LEFT);
+                break;
+            case KeyEvent.VK_RIGHT:
+                controller.handleMovement(Pacman.Direction.RIGHT);
+                break;
+            case KeyEvent.VK_ESCAPE:
+                controller.handlePause();
+                break;
         }
     }
 
