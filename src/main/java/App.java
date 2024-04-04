ec30dd44fb8b329f2b9b80f777a7c2fd3cc54e31
David Douangmarath <david.douangmarath@etu.univ-lyon1.fr>
2024-04-04 08:38:24 +0200

    Revert "Merge branch 'feature/pacmanModel' into 'main'"
    This reverts commit b2469131521ecdc97720722f68ae9edef84b7159, reversing
    changes made to a7204debdafe5c36baa3b99466e85996863301c7.
    
    Revert commit de la branche main

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,19 +1,8 @@
 /**
- * Classe App pour notre application.
+ *
  */
-public final class App {
-    /**
-     * Permet d'éviter que cette classe ne puisse être instancié.
-     */
-    private App() {
-        throw new AssertionError();
-    }
-
-    /**
-     * Début de l'application.
-     * @param args
-     */
-    public static void main(final String[] args) {
+public class App {
+    public static void main(String[] args){
         System.out.println("Bonjour, ça va ?");
     }
 }
