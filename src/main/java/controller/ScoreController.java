d0d299b4f7feb382aca9fb359ec9719802184a93
Loric Audin <loricaudin@yahoo.fr>
2024-04-13 16:02:05 +0200

    Ajout commentaire scoreController

--- src/main/java/controller/ScoreController.java
+++ src/main/java/controller/ScoreController.java
@@ -1,29 +1,29 @@
 package controller;
 
 /**
- * 
+ * Contrôleur pour l'affichage du score
  */
 public class ScoreController {
     
     private Integer count;
     /**
-     * 
+     * Constructeur de la classe ScoreController
      */
     public ScoreController() {
         this.count = 0;
     }
 
     /**
-     * 
+     * Vérifier le score
      * @param nbPoint
-     * @return Boolean
+     * @return boolean
      */
     public boolean control(final Integer nbPoint) {
         return !(this.count < nbPoint);
     }
 
     /**
-     * 
+     * Récupérer le score
      * @return Integer
      */
     public Integer getCount() {
@@ -31,7 +31,7 @@ public class ScoreController {
     }
 
     /**
-     * 
+     * Mettre à jour le score
      * @param count
      */
     public void setCount(final Integer count) {
